a9d4e40ddb025967f34fe9a800dd55d6b5e229c1
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-06-27 15:23:22 +0300

    problem 2 in c++

---
+++ cpp/ps0002/cpp_ps0002.cpp
@@ -0,0 +1,7 @@
+/* Give examples for variables from real life and consider that:
+    - variables should be named in a way that makes sense
+    - variables should has a suitable data type
+    - variables should be initialized with a suitable value
+    - example:
+        int family_members = 8;
+*/
