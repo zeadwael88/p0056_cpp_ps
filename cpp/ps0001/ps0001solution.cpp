3c8db98477b37a16f76a1592941cd62d640615ac
Sarah <149259734+SaraMaher5@users.noreply.github.com>
2024-06-29 21:11:01 +0300

    Add files via upload
    p005E

---
+++ cpp/ps0001/ps0001solution.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	unsigned short num1, num2, num3;
+
+	cin >> num1 >> num2 >> num3;
+	cout << "--------" << "\n" << num1 + num2 + num3;
+	
+	
+	return 0;
+}
+
