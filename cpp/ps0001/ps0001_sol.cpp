c30482323133068f14bf9a292244ce2be7195a2a
Basmalah Osama <basmalahosama11@gmail.com>
2024-06-29 08:39:52 +0300

    problem 1 in C++

---
+++ cpp/ps0001/ps0001_sol.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a ,b ,c ;
+    int s ;
+    cout<< "Enter first number \n" ;
+    cin>> a;
+    cout<< "Enter second number \n" ;
+    cin>> b ;
+     cout<< "Enter third number \n" ;
+     cin>> c ;
+     s= a + b + c ;
+     cout<< " The sum is "<< s << endl ;
+    return 0;
+}
