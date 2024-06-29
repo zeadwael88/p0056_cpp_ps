f03d3b2d7a0b72b1168c3824ec370c4e27147500
EslamElarabawey <eslamarabawey@gmail.com>
2024-06-29 14:42:48 +0300

    problem 1 in c++

---
+++ cpp/ps0001/ps0001_soin_cpp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int x, y, z, f;
+	cin >> x;
+	cin >> y;
+	cin >> z;
+	f = x + y + z;
+	cout << "_______" << endl;
+	cout << f;
+	_getch();
+
+}
