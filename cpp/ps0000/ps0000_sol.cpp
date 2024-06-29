713353656a34a1fff82c8e2e8c37ef35fe48be75
Mohamed Ashraf <107517330+Zero028@users.noreply.github.com>
2024-06-29 19:33:55 +0300

    Add files via upload

---
+++ cpp/ps0000/ps0000_sol.cpp
@@ -0,0 +1,18 @@
+// Replace @ with # when you include a library
+// There's no .h in iostream
+// Library can't be between double quotes
+#include <iostream>
+
+// main must be written ib small letters not capital
+int main(void)
+{
+    // You must put std:: before cout
+    // What comes after std::cout is << not >>
+    // Remove /n since it's not wanted and we only want one line
+    // Each line of code must end with a semicolon
+
+    std::cout << "Hello, World!";
+
+  // You must return 0 in main function
+  return 0;
+}
