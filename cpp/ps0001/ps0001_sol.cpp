e841ade9741012836828390db74e1e27e984f8e5
Mohamed Ashraf <107517330+Zero028@users.noreply.github.com>
2024-06-29 19:38:21 +0300

    Add files via upload

---
+++ cpp/ps0001/ps0001_sol.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,z;
+    cin >> x >> y >> z;
+    int sum=x+y+z;
+    cout << "---------" << endl;
+    cout << sum;
+    return 0;
+}
