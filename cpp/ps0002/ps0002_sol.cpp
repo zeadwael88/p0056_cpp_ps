3ec69d15a24a8b1b93271dacb1c91c298f96d438
Basmalah Osama <basmalahosama11@gmail.com>
2024-06-29 08:42:01 +0300

    problem 2 in C++

---
+++ cpp/ps0002/ps0002_sol.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int residents = 23 ;
+   int alternative = 9 ;
+   short members = 6 ;
+   double price = 5.5 ;
+   char ahmed = 'a' ;
+   long births = 1000000000  ;
+   double division = 3.5 ;
+   long bill = 1234560 ;
+   long age = 12345664 ;
+
+return 0;
+}
