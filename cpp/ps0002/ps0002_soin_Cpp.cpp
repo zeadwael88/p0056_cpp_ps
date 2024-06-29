d0f109230c7317b44bf9523a1008fd111b14654a
EslamElarabawey <eslamarabawey@gmail.com>
2024-06-30 00:11:51 +0300

    problem 3 in c++

---
+++ cpp/ps0002/ps0002_soin_Cpp.cpp
@@ -0,0 +1,9 @@
+short population = 100;
+short number = 2;
+short tim_people = 20;
+float one_price = 5.5;
+char first_char = 'S';
+long number_births= 14120000;
+short output = 7;
+double invoice = 5555.50;
+long age_ = 536786732663;
