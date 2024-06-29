f303f53fb6de68af3f7e0bff755a1882d4be43c4
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-06-29 16:11:57 +0300

    Update ps0001_sol.cpp

--- cpp/ps0001/ps0001_sol.cpp
+++ cpp/ps0001/ps0001_sol.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 int main()
 {
-    int a ,b ,c ;
-    int s ;
-    cout<< "Enter first number \n" ;
+    int a, b, c; // make your code well-formatted
+    // int s ; // you don't have to delare a new variable
+    cout<< "Enter first number\n";
     cin>> a;
-    cout<< "Enter second number \n" ;
+    cout<< "Enter second number\n";
     cin>> b ;
-     cout<< "Enter third number \n" ;
-     cin>> c ;
-     s= a + b + c ;
-     cout<< " The sum is "<< s << endl ;
+    cout<< "Enter third number\n";
+    cin>> c ;
+    // s= a + b + c ; // and assign the result of summation to it
+    cout<< "The sum is "<< a + b + c << endl; // use this instead 
     return 0;
 }
+// the program is user-friendly 😎 8.5/10 ⭐
