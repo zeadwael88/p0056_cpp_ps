18219c9f52b3b3534ad3bbaa29a0d0050fcfd7aa
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-06-30 00:00:15 +0300

    problem 2 marked

--- cpp/ps0002/ps0002_sol.cpp
+++ cpp/ps0002/ps0002_sol.cpp
@@ -4,15 +4,16 @@ using namespace std;
 
 int main()
 {
-   int residents = 23 ;
-   int alternative = 9 ;
-   short members = 6 ;
-   double price = 5.5 ;
+   int residents = 23 ; // unsigned short (if you need to use all range) or short
+   int alternative = 9 ; // unsigned short (if you need to use all range) or short
+   short members = 6 ; 
+   double price = 5.5 ; // float is situable for memory more as it takes only 7 or 6 digits precision
    char ahmed = 'a' ;
    long births = 1000000000  ;
-   double division = 3.5 ;
+   double division = 3.5 ; // or long double
    long bill = 1234560 ;
-   long age = 12345664 ;
+   long age = 12345664 ; // in_seconds_age is better naming
 
-return 0;
+   return 0;
 }
+// examples are pretty good 😎 8.5/10 ⭐
