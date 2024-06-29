35a26d0174a0c98c531e622836f021b78b1cd747
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-06-30 00:52:44 +0300

    problem 2 marked

--- cpp/ps0002/ps0002_soin_Cpp.cpp
+++ cpp/ps0002/ps0002_soin_Cpp.cpp
@@ -1,9 +1,15 @@
-short population = 100;
-short number = 2;
-short tim_people = 20;
-float one_price = 5.5;
-char first_char = 'S';
-long number_births= 14120000;
-short output = 7;
-double invoice = 5555.50;
-long age_ = 536786732663;
+#include<iostream>
+using namespace std;
+int main()
+{
+  short population = 100; // or short int
+  short number = 2; // alters is better for names
+  short tim_people = 20; // "tim_people" isn't better naming
+  float one_price = 5.5;
+  char first_char = 'S';
+  long number_births= 14120000;
+  short output = 7; // double or long double
+  double invoice = 5555.50; 
+  long age_ = 536786732663;	// in_seconds_age is better naming
+}
+// great work for declaring and assigning variables 😎 9/10 ⭐
