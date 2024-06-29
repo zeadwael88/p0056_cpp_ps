6ef9775137ccd0c91ae1fd4419bd836553adc17b
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-06-29 16:04:14 +0300

    Update and rename ps0001_soin_cpp.cpp to ps0001_soln_cpp.cpp

---
+++ cpp/ps0001/ps0001_soln_cpp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int x, y, z;
+	cin >> x;
+	cin >> y;
+	cin >> z;
+	// f = x + y + z; // you don't have to declare a variable and assign it the result for the sum
+	cout << "_______" << endl;
+	// cout << f; // instead you can use
+	cout << x + y + z;
+	_getch();
+}
+// great work 😎 9/10 ⭐
