350a045109f16aff19742c2f0711df8cafb1a4f5
Mohamed Ashraf <107517330+Zero028@users.noreply.github.com>
2024-06-29 21:08:06 +0300

    Add files via upload

---
+++ cpp/ps0002/ps0002_sol.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    // عدد سكان عمارة
+    unsigned int people = 50;
+    // عدد بدائل ادوية
+    unsigned int alternative = 4;
+    // عدد افراد فريق برمجة
+    unsigned int team_members = 20;
+    // سعر مخبوزات
+    float price = 10.75f;
+    // الحرف الأول من اسم شخص
+    char letter = 'M';
+    // عدد المواليد في دولة تعدادها كبير
+    long births = 3000000000;
+    // ناتج اكثر دقة لقسمة رقم علي 7
+    long double divison = 3.14285714286l;
+    // فاتورة الكهربا
+    float bill = 320.5;
+    // عمر شخص بالثانية
+    long age_seconds = 99532800;
+
+
+    return 0;
+}
