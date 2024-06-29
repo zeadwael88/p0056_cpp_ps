f00f6bbd6a9a53eb0340429ea4657c118e915221
Sarah <149259734+SaraMaher5@users.noreply.github.com>
2024-06-29 19:48:18 +0300

    Add files via upload
    p005E

---
+++ cpp/ps0000/ps0000solution.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream> 
+using namespace std;
+int main(void)
+{
+	cout << "Hello, World!";
+		return 0;
+}
+
+
+
+
+/*@include <"iostream.h"> in c++ the first statement should start with # its a preprocessor directive , inside the <> it should not contain double quotation
+ so it should be written like this  #include <iostream> 
+
+
+int Main(void) when i tried to run with the void it didn't give me any error(idk the reason) so i kept it 
+{
+	cout >> "Hello, World!/n"  if we just want to print a Hello, World! , so it should be written as it is
+		return 0; ( i edited it from return 1 to return 0 as the void don't return anything)
+} */
