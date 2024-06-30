25b99a453165deaef6c8e03a737c43b6c75a2e95
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-06-30 09:51:35 +0300

    problem 2 marked

--- cpp/ps0002/ps0002_sol.cpp
+++ cpp/ps0002/ps0002_sol.cpp
@@ -3,11 +3,11 @@
 int main()
 {
     // عدد سكان عمارة
-    unsigned int people = 50;
+    unsigned int people = 50; // short
     // عدد بدائل ادوية
-    unsigned int alternative = 4;
+    unsigned int alternative = 4; // short
     // عدد افراد فريق برمجة
-    unsigned int team_members = 20;
+    unsigned int team_members = 20; // short
     // سعر مخبوزات
     float price = 10.75f;
     // الحرف الأول من اسم شخص
@@ -16,11 +16,12 @@ int main()
     long births = 3000000000;
     // ناتج اكثر دقة لقسمة رقم علي 7
     long double divison = 3.14285714286l;
-    // فاتورة الكهربا
-    float bill = 320.5;
+    // فاتورة الكهربا لخط انتاج
+    float bill = 320.5; // double is valid for some special cases
     // عمر شخص بالثانية
     long age_seconds = 99532800;
 
 
     return 0;
 }
+// great work in declaring, naming and assigning variables with the suitable data type and modifier 😎 9/10 ⭐
