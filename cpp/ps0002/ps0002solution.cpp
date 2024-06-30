ac91a740ec745b71e90c2aa0a4b9f8ac9deef224
Sarah <149259734+SaraMaher5@users.noreply.github.com>
2024-06-30 18:59:42 +0300

    Add files via upload

---
+++ cpp/ps0002/ps0002solution.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+   {
+  unsigned short building_resident = 1;   //عدد سكان عمارة
+
+unsigned short alternative_medicines = 5;  //عدد البدايل لدوا في الصيدلية
+
+string names_of_team = "ahmed ,mohamed ,ali ,mariam";  //أفراد تيم في شركة برمجة
+
+ float price_of_bread = 7.5;  //سعر واحدة من مخبوزات الفرن
+
+ char first_letter = 'A';  //الحرف الاول من اسم شخص
+
+ unsigned long long births_of_the_day = 2100;  //عدد المواليد في دولة تعدادها كبير زي الصين
+
+ double divide_13_by7 = 1.857142857142857;  //ناتج اكثر دقة لقسمة رقم عىل 7
+
+ long double electricity_bill = 632.770;  //فاتورة الكهربا لخط انتاج في مصر
+
+ long long age_in_sec = 1200000;  //عمر أشخاص كبار و صغار ف السن بالثانية
+
+  return 0;
+
+     }
+
+     
