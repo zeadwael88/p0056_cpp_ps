51aa381b9c779417d86520b847fcb5b02a846b87
zeadwael88 <zeadwael124@gmail.com>
2024-07-01 14:54:16 +0300

    problem 1 in c++

---
+++ cpp/ps0001/p0056.so1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x , y , o ;
+
+    cout << "please enter x and y and o \n";
+    cin >> x >> y >> o ;
+
+    cout << "total = " << x + y + o << "\n" ;
+
+    return 0;
+}
