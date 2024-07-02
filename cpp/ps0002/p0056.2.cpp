984ff30d3bae526a8dc033740d37a857527f40e6
zeadwael88 <zeadwael124@gmail.com>
2024-07-02 14:14:53 +0300

    problem 2 in c ++

---
+++ cpp/ps0002/p0056.2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int home  20 ;
+
+    int  pharmase   3;
+
+    double  progrem   200;
+
+    float bekary    5.5;
+
+    char a;
+
+    double china    100000000;
+
+    float
+
+    double fatora    12462422 ;
+
+    double age ;
+
+
+    return 0;
+}
