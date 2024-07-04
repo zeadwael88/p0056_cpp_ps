a11c2f3384a90dba06709f9eaabfe4bdb7eef2c6
zeadwael88 <zeadwael124@gmail.com>
2024-07-04 15:27:04 +0300

    problem 3 in c ++

---
+++ cpp/ps0003/p0056.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int number ;
+   cout << "enter number \n" ;
+   cin >> number ;
+
+    if (number % 2 == 0)
+        cout << "Number is even \n";
+   else
+        cout << "Number is odd \n";
+
+
+
+    return 0;
+}
+
