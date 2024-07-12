5de09b3b4f83b3c58db3aa1e24985d13da9e0ae9
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-07-13 00:42:16 +0300

    typos

--- cpp/ps0006/cpp_ps0006.cpp
+++ cpp/ps0006/cpp_ps0006.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-void spectial_counter(int end, int start, int step);
+void special_counter(int end, int start, int step);
 
 int main()
 {
-    spectial_counter(20);
-    spectial_counter(20, 10);
-    spectial_counter(20, 10, 5);
-    spectial_counter(10, 20, -5);
+    special_counter(20);
+    special_counter(20, 10);
+    special_counter(20, 10, 5);
+    special_counter(10, 20, -5);
     
-    // spectial_counter(20, 10, -5); // error
+    // special_counter(20, 10, -5); // error
 
     return 0;
 }
