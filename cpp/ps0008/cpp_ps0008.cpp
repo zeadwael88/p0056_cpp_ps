86f71c924a07af54c0a9c5ffedc550aaa0167465
Moustafa Yasser <moustafa.yasser.1212@gmail.com>
2024-08-01 21:09:25 +0300

    problem 8

---
+++ cpp/ps0008/cpp_ps0008.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits>
+#include <cctype>
+#include <string>
+
+void decrypt(std::string msg, int key);
+
+int main(void)
+{
+    // prompting user for valid ciphertext and key
+    
+
+    // invoke your decrypt function to return the plaintext
+
+
+    return 0;
+}
+
+void decrypt(std::string msg, int key)
+{
+    /* implement your function and consider that:
+        - all uppercase letters in ciphertext are uppercase letters in plaintext
+        - all lowercase letters in ciphertext are lowercase letters in plaintext
+        - all digits, special characters, spaces in ciphertext are the same in plaintext
+    */
+   
+}
